902c812ec7729266f633a4738f108780f885436d
Diguin20 <113214657+Diguin20@users.noreply.github.com>
2023-01-24 21:31:07 -0300

    Add files via upload

---
+++ script do trabalho em grupo.sql
@@ -0,0 +1,42 @@
+create database GOT;
+use GOT;
+
+create table personagens(personagem varchar(100),
+     ator_atriz varchar(100),
+     aparicoes int,
+     primeira_apr int(10),
+     ultima_apr int(10),
+     id int auto_increment not null,
+     primary key(id));
+
+create table episodios(
+    Temporada varchar(10),
+    episodios int,
+	titulos varchar(100),
+	data_lanc date,
+    avaliacao int,
+    votos int(10),
+    duracao int(10),
+    diretor varchar(100),
+	id int auto_increment not null,
+    primary key(id));
+    
+    select * from episodios;
+    select * from personagens;
+    
+	
+    select * from personagens where aparicoes limit 5;
+    
+    select * from episodios where temporada =1 order by  duracao desc limit 1;
+    select * from episodios where temporada =2 order by  duracao desc limit 1;
+    select * from episodios where temporada =3 order by  duracao desc limit 1;
+    select * from episodios where temporada =4 order by  duracao desc limit 1;
+    select * from episodios where temporada =5 order by  duracao desc limit 1;
+    select * from episodios where temporada =6 order by  duracao desc limit 1;
+    select * from episodios where temporada =7 order by  duracao desc limit 1;
+    select * from episodios where temporada =8 order by  duracao desc limit 1;
+    
+    select * from episodios order by votos desc limit 5;
+    
+    
+    
